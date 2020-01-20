66ae69bf8ad916ced106a6f5054831a15350efee
Konstantin Pritychenko <k.pritychenko@zarplata.ru>
2020-01-21 00:58:31 +0700

    Updates

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-// asd
+const cacheName = 'v1';
+const cacheFiles = [
+    './'
+];
+
+self.addEventListener('install', e => {
+    console.log('SW installed');
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log('SW cachin');
+
+                cache.addAll(cacheFiles);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activete', e => {
+    console.log('SW activeted');
+});
