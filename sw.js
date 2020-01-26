f326a2bee54b5aa5b9d095c6ad990789d96a4793
Konstantin Pritychenko <k.pritychenko@zarplata.ru>
2020-01-26 14:48:51 +0700

    Updates

--- sw.js
+++ sw.js
@@ -26,11 +26,15 @@ self.addEventListener('fetch', e => {
     e.respondWith(
         fetch(e.request)
             .then(res => {
+                const resClone = res.clone();
+
                 caches
                     .open(cacheName)
                     .then(cache => {
-                        cache.put(e.request, res.clone())
+                        cache.put(e.request, resClone)
                     })
-            }).catch(() => caches.match(e.request))
+
+                return res;
+            }).catch(() => caches.match(e.request).then(res => res))
     );
 });
